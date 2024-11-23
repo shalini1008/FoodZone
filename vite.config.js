ca6e2efde0bc0a744fd1ae0c180f0c5416f72569
Shalini <122449558+shalini1008@users.noreply.github.com>
2024-11-23 18:55:15 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,9 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      "/api": "http://localhost:9000", // Proxy for Express API
-    },
-  },
   build: {
-    outDir: "dist",
+    outDir: 'dist',  // Ensure the output is in the dist folder
   },
 });
