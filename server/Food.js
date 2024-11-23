98e0e435708a4637a54ab9d70b09ab607c4eb5c7
Shalini <122449558+shalini1008@users.noreply.github.com>
2024-11-23 18:45:04 +0530

    Update Food.js

--- server/Food.js
+++ server/Food.js
@@ -1,51 +1,50 @@
-// /api/food.js
 export default async function handler(req, res) {
   const foodData = [
     {
       name: "Boiled Egg",
       price: 10,
       text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
-      image: "/images/egg.png",  // Correct path
+      image: "/images/egg.png", 
       type: "breakfast",
     },
     {
       name: "RAMEN",
       price: 25,
       text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
-      image: "/images/ramen.png",  // Correct path
+      image: "/images/ramen.png", 
       type: "lunch",
     },
     {
       name: "GRILLED CHICKEN",
       price: 45,
       text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
-      image: "/images/chicken.png",  // Correct path
+      image: "/images/chicken.png", 
       type: "dinner",
     },
     {
       name: "CAKE",
       price: 18,
       text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
-      image: "/images/cake.png",  // Correct path
+      image: "/images/cake.png", 
       type: "breakfast",
     },
     {
       name: "BURGER",
       price: 23,
       text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
-      image: "/images/burger.png",  // Correct path
+      image: "/images/burger.png",  
       type: "lunch",
     },
     {
       name: "PANCAKE",
       price: 25,
       text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
-      image: "/images/pancake.png",  // Correct path
+      image: "/images/pancake.png",  
       type: "dinner",
     },
   ];
 
-  res.status(200).json(foodData);  // Respond with the food data
+  res.status(200).json(foodData);  
 }
 
 
