3fb46896a9e277aef3610a13ced3cb1a5c064ae2
Shalini <122449558+shalini1008@users.noreply.github.com>
2024-11-23 16:58:56 +0530

    Update Food.js

--- server/Food.js
+++ server/Food.js
@@ -1,51 +1,50 @@
-// This file acts as your backend API endpoint
-import path from "path";
-
+// /api/food.js
 export default async function handler(req, res) {
   const foodData = [
     {
       name: "Boiled Egg",
       price: 10,
       text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
-      image: "/images/egg.png",
+      image: "/public/images/egg.png",  // Ensure the correct path to images
       type: "breakfast",
     },
     {
       name: "RAMEN",
       price: 25,
       text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
-      image: "/images/ramen.png",
+      image: "/public/images/ramen.png",
       type: "lunch",
     },
     {
       name: "GRILLED CHICKEN",
       price: 45,
       text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
-      image: "/images/chicken.png",
+      image: "/public/images/chicken.png",
       type: "dinner",
     },
     {
       name: "CAKE",
       price: 18,
       text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
-      image: "/images/cake.png",
+      image: "/public/images/cake.png",
       type: "breakfast",
     },
     {
       name: "BURGER",
       price: 23,
       text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
-      image: "/images/burger.png",
+      image: "/public/images/burger.png",
       type: "lunch",
     },
     {
       name: "PANCAKE",
       price: 25,
       text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
-      image: "/images/pancake.png",
+      image: "/public/images/pancake.png",
       type: "dinner",
     },
   ];
 
   res.status(200).json(foodData);  // Respond with the food data
 }
+
